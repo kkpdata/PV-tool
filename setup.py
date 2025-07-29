930f599e27038eb378cbabca9c9ae90e8080f793
TjaldaDeenekamp <tjalda.deenekamp@arcadis.com>
2025-07-29 10:56:28 +0200

    new wheel test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PV-tool',
-    version='0.1.0',
+    version='0.1.1',
     author='Arcadis',
     author_email='leo.kwakman@arcadis.com',
     description='Functionalities concerning the PV-tool.',
