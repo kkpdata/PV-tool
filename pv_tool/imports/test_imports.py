0dba0850b975fb2a0a8be0a465b00144b1c3e2d3
TjaldaDeenekamp <tjalda.deenekamp@arcadis.com>
2025-07-29 11:07:00 +0200

    eerste opzet unittest

---
+++ pv_tool/imports/test_imports.py
@@ -0,0 +1,9 @@
+import unittest
+from pv_tool.imports.import_data import Dbase
+
+def test_import_data_and_validate() -> bool:
+    dbase = Dbase()
+
+    stowa_dir = ...
+    export_path = ...
+    dbase.import_data_and_validate(source='Stowa', source_dir=stowa_dir, export_path=export_path)
