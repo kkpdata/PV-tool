8d6efe5aef5218ebe82ffb39d5abf18d87affe8d
TjaldaDeenekamp <tjalda.deenekamp@arcadis.com>
2025-12-12 13:41:00 +0100

    unittest cphi

--- pv_tool/cphi_analysis/test_c_phi_analysis.py
+++ pv_tool/cphi_analysis/test_c_phi_analysis.py
@@ -14,74 +14,41 @@ export_dir = make_temp_folder(
 export_dir = Path(export_dir)
 
 
-def test_cphi_txt_analyse():
+def test_cphi_analyse():
     """Test implemented. Do not change input."""
     dbase = Dbase()
     source_dir = Path(os.path.join(FILE_PATH, "Dbase.xlsx"))
     dbase.import_data(source="Dbase", source_dir=source_dir)
     dbase.export_dbase_to_excel(export_dir=export_dir)
 
-    # Initialize analysis # TODO: loop door analysis_type in toekomst, onderstaande functies kunnen dan weg.
-    analyse = CPhiAnalyse(
-        dbase=dbase,
-        investigation_groups=['TXT_SAFE_klei_licht_16_175'],
-        effective_stress='15% rek',
-        analysis_type='TXT_CPhi'
-    )
-    # apply settings
-    analyse.apply_settings(alpha=0.75)
-    # apply parameters
-    analyse.apply_parameters(cohesie_gem=8.0, phi_kar=0.53, cohesie_kar=6.72)
-    # print results
-    analyse.print_short_results()
-    # exports
-    file_name = 'Template_PVtool5_0.xlsx'
-    analyse.add_results_to_dbase(path=export_dir, file_name=file_name)
-    analyse.save_to_pdf(path=export_dir)
+    # Initialize analysis
+    analysis_types = ['TXT_CPhi', 'TXT_SH', 'DSS_CPhi', 'DSS_SH']
+    for analysis_type in analysis_types:
+        analyse = CPhiAnalyse(
+            dbase=dbase,
+            investigation_groups=['TXT_SAFE_klei_licht_16_175'],
+            effective_stress='15% rek',
+            analysis_type=analysis_type
+        )
+
+        if analysis_type == 'TXT_CPhi':
+            # apply settings
+            analyse.apply_settings(alpha=0.75)
+            # apply parameters
+            analyse.apply_parameters(cohesie_gem=8.0, phi_kar=0.53, cohesie_kar=6.72)
+            # print results
+            analyse.print_short_results()
+            # exports
+            file_name = 'Template_PVtool5_0.xlsx'
+            analyse.add_results_to_dbase(path=export_dir, file_name=file_name)
+            analyse.save_to_pdf(path=export_dir)
+
     # remove temp_folder
     shutil.rmtree(export_dir)
+    return True
 
 
-def test_cphi_dss_analyse():
-    """Test implemented. Do not change input."""
-    dbase = Dbase()
-    source_dir = Path(os.path.join(FILE_PATH, "Dbase.xlsx"))
-    dbase.import_data(source="Dbase", source_dir=source_dir)
+class TestImportAndValidate(unittest.TestCase):
 
-    # Initialize analysis
-    _ = CPhiAnalyse(
-        dbase=dbase,
-        investigation_groups=['TXT_SAFE_klei_licht_16_175'],
-        effective_stress='20% rek',
-        analysis_type='DSS_CPhi'
-    )
-
-
-def test_cphi_txt_sh_analyse():
-    """Test implemented. Do not change input."""
-    dbase = Dbase()
-    source_dir = Path(os.path.join(FILE_PATH, "Dbase.xlsx"))
-    dbase.import_data(source="Dbase", source_dir=source_dir)
-
-    # Initialize analysis
-    _ = CPhiAnalyse(
-        dbase=dbase,
-        investigation_groups=['DSS_SAFE_veen'],
-        effective_stress='15% rek',
-        analysis_type='TXT_SH'
-    )
-
-
-def test_cphi_dss_sh_analyse():
-    """Test implemented. Do not change input."""
-    dbase = Dbase()
-    source_dir = Path(os.path.join(FILE_PATH, "Dbase.xlsx"))
-    dbase.import_data(source="Dbase", source_dir=source_dir)
-
-    # Initialize analysis
-    _ = CPhiAnalyse(
-        dbase=dbase,
-        investigation_groups=['DSS_SAFE_veen'],
-        effective_stress='20% rek',
-        analysis_type='DSS_SH'
-    )
+    def test_import_stowa_data(self):
+        self.assertTrue(test_cphi_analyse())
